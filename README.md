63a88758f810ad4ddc560f6ae8e8fc02dc2b1c64
Abhinav Sharma <abhi18av@gmail.com>
2017-10-31 22:10:05 +0530

    working on the postgresql database

--- README.md
+++ README.md
@@ -43,3 +43,9 @@ Copyright © 2017 FIXME
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
 
+
+OrientDB console v.2.2.29 (build 9914189f972103907c24377a1567897e68642920) https://www.orientdb.com
+Type 'help' to display all the supported commands.
+Installing extensions for GREMLIN language v.2.6.0
+
+postgres (PostgreSQL) 10.0
--- project.clj
+++ project.clj
@@ -1,11 +1,17 @@
 (defproject compass "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A clojure compass for OrientDB"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [  [org.clojure/clojure "1.9.0-beta3"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta3"]
+
+                ;; To test how the postgresql database abstraction works
+               [com.layerware/hugsql "0.4.8"]
+               [org.postgresql/postgresql "42.1.4"]
+  
+             ;; to simplify the Data Structure traversals
   				   ;[com.rpl/specter "1.0.4"][medley "1.0.0"]
-  				   [com.orientechnologies/orientdb-core "2.2.29"] ]
+             [com.orientechnologies/orientdb-core "2.2.29"] ]
   :main ^:skip-aot compass.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
