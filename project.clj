0d4ff4177ae2d4935d9f0b28afbeb7683f1e9c01
Abhinav Sharma <abhi18av@gmail.com>
2017-10-31 21:56:37 +0530

    added the orientdb driver

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [  [org.clojure/clojure "1.9.0-beta3"]
+  				   ;[com.rpl/specter "1.0.4"][medley "1.0.0"]
+  				   [com.orientechnologies/orientdb-core "2.2.29"] ]
   :main ^:skip-aot compass.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
